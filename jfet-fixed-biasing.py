c8087def52b5e6e64c77d9f11e20b69feea56677
bualoydgreat <37010012+bualoydgreat@users.noreply.github.com>
2021-03-21 13:58:07 +0800

    Update jfet-fixed-biasing.py
    Revised UI descriptions and formulas.

--- jfet-fixed-biasing.py
+++ jfet-fixed-biasing.py
@@ -10,9 +10,13 @@ ax = plt.axes()
 
 #tkinter GUI
 master = tk.Tk()
+master.title("JFET Fixed Bias Point Calculator")
 tk.Label(master, text="Vgg").grid(row=0)
 tk.Label(master, text="Vp").grid(row=1)
 tk.Label(master, text="idss").grid(row=2)
+tk.Label(master, text="V").grid(row=0, column=2)
+tk.Label(master, text="V").grid(row=1, column=2)
+tk.Label(master, text="mA").grid(row=2, column=2)
 
 e1 = tk.Entry(master)
 e2 = tk.Entry(master)
@@ -24,29 +28,29 @@ e3.grid(row=2, column=1)
 
 
 def fixed_biasing():
-    Vgg = float(e1.get())
+    Vgg = float(e1.get())*(-1)
     Vp = float(e2.get())
     idss = float(e3.get())
-    idq = idss*(1-(Vgg/Vp))**2
-    Vgsq = Vgg
-    tk.Label(master, text="idq: " + str(idq)).grid(row=3,
+    idq = round(idss*(1-(Vgg/Vp))**2, 4)
+    Vgsq = -Vgg
+    tk.Label(master, text="idq: " + str(idq) + "mA").grid(row=3,
                                                   column=1,
                                                   )
-    tk.Label(master, text="Vgsq: " + str(Vgsq)).grid(row=4,
+    tk.Label(master, text="Vgsq: " + str(Vgsq) + "V").grid(row=4,
                                                   column=1,
                                                   )
 
 
 #plot
 def fixed_plot():
-    Vgg = float(e1.get())
+    Vgg = float(e1.get())*(-1)
     Vp = float(e2.get())
     idss = float(e3.get())
     x = np.linspace(Vp, 1, 100)
     x_conv = np.asarray(x)/Vp
     y = idss*(1-(x_conv))**2
     idq = idss*(1-(Vgg/Vp))**2
-    Vgsq = Vgg    
+    Vgsq = Vgg
     plt.plot(x, y, '-r', label='Transfer Curve')
     plt.axvline(x=Vgg, color='g', label='Fixed Vgsq')
     plt.plot(Vgsq, idq, 'bo', label='idq Intersection')
@@ -54,7 +58,6 @@ def fixed_plot():
     plt.ylabel('idq', color='#1C2833')
     plt.legend(loc='upper left')
     plt.show()
-    plt.gcf().show()
 
 
 tk.Button(master,
@@ -78,4 +81,5 @@ tk.Button(master,
 
 
 master.mainloop()
+
 
